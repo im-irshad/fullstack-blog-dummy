7220d358e158e66da153312e11cb6907c2a91cfd
Irshad Muhammad <imabbasi@msn.com>
2022-07-23 15:34:48 +0200

    added react app, added single blog route

--- api/routes/Blogs.js
+++ api/routes/Blogs.js
@@ -2,13 +2,20 @@ const express = require("express");
 const router = express.Router();
 const { Blogs } = require("../models");
 
-router.get("/", (req, res) => {
-  res.send("Hello from Blogs");
+router.get("/", async (req, res) => {
+  const listOfBlogs = await Blogs.findAll();
+  res.json(listOfBlogs);
 });
 
 router.post("/", async (req, res) => {
   const blog = req.body;
   await Blogs.create(blog);
   res.json(blog);
+});
+
+router.get("/:id", async (req, res) => {
+  const blog = req.params.id;
+  const foundBlog = await Blogs.findOne({ where: { id: blog } });
+  res.json(foundBlog);
 });
 module.exports = router;
