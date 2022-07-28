93a050838254446b76471efe687a3358380c980f
Irshad Muhammad <imabbasi@msn.com>
2022-07-28 11:06:01 +0200

    added mainFeaturedPost

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Container,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
+import { Fragment } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import MainFeaturedPost from "./components/MainFeaturedPost";
+import FeaturedPost from "./components/FeaturedPost";
+import Sidebar from "./components/Sidebar";
+
+const sections = [
+  { title: "Technology", url: "#" },
+  { title: "Design", url: "#" },
+  { title: "Culture", url: "#" },
+  { title: "Business", url: "#" },
+  { title: "Politics", url: "#" },
+  { title: "Opinion", url: "#" },
+  { title: "Science", url: "#" },
+  { title: "Health", url: "#" },
+  { title: "Style", url: "#" },
+  { title: "Travel", url: "#" },
+];
+const mainFeaturedPost = {
+  title: "Title of a longer featured blog post",
+  description:
+    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+  image: "https://source.unsplash.com/random",
+  imageText: "main image description",
+  linkText: "Continue reading…",
+};
+
+const theme = createTheme();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="lg">
+        <Header title="Blog" sections={sections} />
+        <MainFeaturedPost post={mainFeaturedPost} />
+        <FeaturedPost />
+        <Sidebar />
+      </Container>
+    </ThemeProvider>
   );
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Toolbar from "@mui/material/Toolbar";
+import { Button, IconButton, Typography } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import Link from "@mui/material/Link";
 
-function Header() {
-  return <div>Header</div>;
+function Header({ sections, title }) {
+  return (
+    <Fragment>
+      <Toolbar sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Button size="small">Subscribe</Button>
+        <Typography
+          component="h2"
+          variant="h5"
+          color="inherit"
+          align="center"
+          noWrap
+          sx={{ flex: 1 }}
+        >
+          {title}
+        </Typography>
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+        <Button variant="outlined" size="small">
+          Sign up
+        </Button>
+      </Toolbar>
+      <Toolbar
+        component="nav"
+        variant="dense"
+        sx={{ justifyContent: "space-between", overflowX: "auto" }}
+      >
+        {sections.map((section) => (
+          <Link
+            color="inherit"
+            noWrap
+            key={section.title}
+            variant="body2"
+            href={section.url}
+            sx={{ p: 1, flexShrink: 0 }}
+          >
+            {section.title}
+          </Link>
+        ))}
+      </Toolbar>
+    </Fragment>
+  );
 }
 
 export default Header;
