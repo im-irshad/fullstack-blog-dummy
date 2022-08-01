0d28e1fc6bee0632eadee524ed8afa99faf025f0
Irshad Muhammad <imabbasi@msn.com>
2022-08-01 11:33:48 +0200

    all pages updated

--- client/src/pages/Travel.js
+++ client/src/pages/Travel.js
@@ -1,7 +1,78 @@
+import { Grid } from "@mui/material";
 import React from "react";
+import FeaturedPost from "../components/FeaturedPost";
+import Main from "../components/Main";
+import MainFeaturedPost from "../components/MainFeaturedPost";
+import Sidebar from "../components/Sidebar";
+import post1 from "../blog-post.1.md";
+import post2 from "../blog-post.2.md";
+import post3 from "../blog-post.3.md";
+
+const posts = [post1, post2, post3];
 
 function Travel() {
-  return <div>Travel</div>;
+  const featuredPosts = [
+    {
+      title: "Featured post",
+      date: "Nov 12",
+      description:
+        "This is a wider card with supporting text below as a natural lead-in to additional content.",
+      image: "https://source.unsplash.com/random",
+      imageLabel: "Image Text",
+    },
+    {
+      title: "Post title",
+      date: "Nov 11",
+      description:
+        "This is a wider card with supporting text below as a natural lead-in to additional content.",
+      image: "https://source.unsplash.com/random",
+      imageLabel: "Image Text",
+    },
+  ];
+  const mainFeaturedPost = {
+    title: "Title of a longer featured blog post",
+    description:
+      "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+    image: "https://source.unsplash.com/random",
+    imageText: "main image description",
+    linkText: "Continue reading…",
+  };
+  const sidebar = {
+    title: "About",
+    description:
+      "Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.",
+    archives: [
+      { title: "March 2020", url: "" },
+      { title: "February 2020", url: "#" },
+      { title: "January 2020", url: "#" },
+      { title: "November 1999", url: "#" },
+      { title: "October 1999", url: "#" },
+      { title: "September 1999", url: "#" },
+      { title: "August 1999", url: "#" },
+      { title: "July 1999", url: "#" },
+      { title: "June 1999", url: "#" },
+      { title: "May 1999", url: "#" },
+      { title: "April 1999", url: "#" },
+    ],
+  };
+  return (
+    <>
+      <MainFeaturedPost post={mainFeaturedPost} />
+      <Grid container spacing={4}>
+        {featuredPosts.map((post) => (
+          <FeaturedPost key={post.title} post={post} />
+        ))}
+      </Grid>
+      <Grid container spacing={5} sx={{ mt: 3 }}>
+        <Main title="From the firehose" posts={posts} />
+        <Sidebar
+          title={sidebar.title}
+          description={sidebar.description}
+          archives={sidebar.archives}
+        />
+      </Grid>
+    </>
+  );
 }
 
 export default Travel;
